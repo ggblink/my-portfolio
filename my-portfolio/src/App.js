c163922b58721ceb018acf18e351116a2626d569
link <3246383288@qq.com>
2025-08-22 10:27:30 +0800

    Initialize my-portfolio folder

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, I'm Mirco!</h1>
+      <p>This is my personal homepage deployed with Vercel.</p>
+      <a
+        href="https://github.com/your-github"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        My GitHub
+      </a>
+    </div>
+  );
+}
+
+export default App;
